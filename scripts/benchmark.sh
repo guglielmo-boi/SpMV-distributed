08fde66c76e7d32943707a99cc8fe72a81a9ec32
Guglielmo Boi <boi.guglielmo@gmail.com>
2026-07-27 10:33:50 +0200

    Changed benchmark script

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -3,14 +3,15 @@
 #SBATCH --nodelist=edu01
 #SBATCH --account=gpu.computing26
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --gres=gpu:1
+#SBATCH --ntasks=4
+#SBATCH --gres=gpu:4
 #SBATCH --cpus-per-task=1
 #SBATCH --time=02:00:00
 
 #SBATCH --job-name=spmv-benchmark
-#SBATCH --output=spmv-benchmark-%j.out
-#SBATCH --error=spmv-benchmark-%j.err
+#SBATCH --array=1-10
+#SBATCH --output=spmv-benchmark-%A_%a.out
+#SBATCH --error=spmv-benchmark-%A_%a.err
 
 module load CUDA/12.1.1
 module load OpenMPI/4.1.5-GCC-12.3.0
